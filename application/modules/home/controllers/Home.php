b7fd3bae99dcffa0e6f357ffca289f2f428a6401
M. Syamsul <58071759+syamban14@users.noreply.github.com>
2024-09-26 14:29:50 +0700

    Update Home.php

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -2,19 +2,25 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 	class Home extends MX_Controller{
 		function __construct(){
-			parent::__construct();
+			// parent::__construct();
 			$this->load->model('Home_m');
-			if($this->session->userdata('logged_in')!=TRUE)
-	        {
-	        	redirect('login','refresh');
-	        }
+			// if($this->session->userdata('logged_in')!=TRUE)
+	  //       {
+	  //       	redirect('login','refresh');
+	  //       }
 		}
 
 		public function index(){
-			if ($this->session->userdata('role')=='murid') {
-				$this->load->view('home_v');
-			}elseif ($this->session->userdata('role')=='ustadz') {
-				$this->load->view('home_ustadz_v');
-			}
+
+			public function index(){
+			$this->load->view('commons/header');
+			$this->load->view('home_v');
+			$this->load->view('commons/footer');
+		}
+			// if ($this->session->userdata('role')=='murid') {
+			// 	$this->load->view('home_v');
+			// }elseif ($this->session->userdata('role')=='ustadz') {
+			// 	$this->load->view('home_ustadz_v');
+			// }
 		}
 	}
